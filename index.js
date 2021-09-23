563a5261fe4f69767031dc09d166edca3f7fb9c2
nori-ok <ayumanodev@gmail.com>
2021-09-23 15:14:45 +0900

    配列処理　map

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * mapやfilterを使った配列の処理
+ */
+
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++) {
+// console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//     return name;
+// });
+
+// console.log(nameArr2);
+
+nameArr.map((name) => console.log(name));
